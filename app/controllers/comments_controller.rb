5b83f9c7c17f16c6e6895b56af1e932dd57ef1a9
akamatsukouta <kouta0618_5fam2000@icloud.com>
2021-03-27 17:22:44 +0900

    first commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,27 @@
+class CommentsController < ApplicationController
+
+  def create
+         #@prototype = Prototype.find(params[:prototype_id])
+         @comment = Comment.new(comment_params)
+      if @comment.save
+         redirect_to "/prototypes/#{@comment.prototype.id}"
+      else
+         
+         render "prtotypes/show"
+      end
+  end
+
+  def show
+   @comments = Comment.all
+  end
+
+  
+
+ 
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text, :comment).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
+  end
+
+end
